f854d52992ec64d821f9f3e4069db9020a245dab
KranticNenad <kranticnenad@gmail.com>
2019-03-13 18:06:57 +0100

    implemented all http requests in employees

--- FrontEnd/Angular/CompanyVehicleService/src/app/employee/employee.component.ts
+++ FrontEnd/Angular/CompanyVehicleService/src/app/employee/employee.component.ts
@@ -12,16 +12,24 @@ export class EmployeeComponent implements OnInit {
     employees: Employee[];
     employee1: Employee;
     employee2: Employee = {employeeId:123, name:"Tamir", surname:"Abdelmalek", dob: new Date(1750,11,25), salary: 4325.23, about: "I AM THE BEST TECH"};
-
+    employee3: Employee = {employeeId:123, name:"Chris", surname:"Abdelmalek", dob: new Date(1750,11,25), salary: 4325.23, about: "I AM THE BEST TECH"};
+    
     public constructor(private employeeService: EmployeeService) {
     }
 
     post(){
       this.employeeService.postEmployee(this.employee2).subscribe(employee => this.employee1);
+      this.employees.push(this.employee2);
     }
 
     delete(){
-      this.employeeService
+      this.employeeService.deleteEmployee(123).subscribe();
+      this.employees = this.employees.filter(Employee => Employee.employeeId != 123);
+    }
+
+    put(){
+      this.employeeService.putEmployee(this.employee3).subscribe(employee => this.employee3);
+      this.employees[this.employees.findIndex(Employee => Employee.employeeId == this.employee3.employeeId)] = this.employee3;
     }
 
     ngOnInit() {
