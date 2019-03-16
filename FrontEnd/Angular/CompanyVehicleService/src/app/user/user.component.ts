d9bb987e6e5d5e90171e7359b140a921e7f030ae
KranticNenad <kranticnenad@gmail.com>
2019-03-16 16:52:26 +0100

    User implemented full functionality

--- FrontEnd/Angular/CompanyVehicleService/src/app/user/user.component.ts
+++ FrontEnd/Angular/CompanyVehicleService/src/app/user/user.component.ts
@@ -1,4 +1,9 @@
+import { UserService } from './user.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { User } from './user.interface';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-user',
@@ -11,7 +16,87 @@ export class UserComponent implements OnInit {
   visible2: boolean = false;
   visible3: boolean = false;
 
+  users:User[];
+  userToDelete: string;
+
+  public constructor(private userService: UserService) {
+  }
+
+  addForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    enabled: new FormControl(),
+    authority: new FormControl(''),
+    email: new FormControl('')
+  });
+  editForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    enabled: new FormControl(),
+    authority: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  saveAsPdf() {
+
+    var doc = new jsPDF('l', 'mm', 'a4');
+    let rows = [];
+
+    this.users.forEach(user => {
+      rows.push([user.username, user.password, user.enabled, user.authority, user.email]);
+    });
+
+    doc.autoTable({
+      head:[["Username","Password","Enabled","Authority","Email"]],
+      body:rows
+    });
+    doc.save("Users.pdf");
+  }
+
+  addUser() {
+    let username : string = this.addForm.get('username').value;
+    let password  : string = this.addForm.get('password').value;
+    let enabled : boolean = this.addForm.get('enabled').value;
+    let authority: string = this.addForm.get('authority').value;
+    let email : string = this.addForm.get('email').value;
+
+    let userToAdd : User = {username:username, password: password, enabled: enabled, authority: authority, email:email};
+
+    this.userService.postUser(userToAdd).subscribe(user => userToAdd);
+    this.users.push(userToAdd);
+    this.visible = false;
+  }
+
+  clickedOnUser(user:User){
+    this.editForm.setValue({username: user.username, password: user.password, enabled: user.enabled.toString(), authority: user.authority, email: user.email});
+  }
+
+  editUser(){
+    let username : string = this.editForm.get('username').value;
+    let password  : string = this.editForm.get('password').value;
+    let enabled : boolean = this.editForm.get('enabled').value;
+    let authority: string = this.editForm.get('authority').value;
+    let email : string = this.editForm.get('email').value;
+
+    let userToEdit : User = {username:username, password: password, enabled: enabled, authority: authority, email:email};
+
+    this.userService.putUser(userToEdit).subscribe(user => userToEdit);
+    this.users[this.users.findIndex(User => User.username == userToEdit.username)] = userToEdit;
+    this.visible2 = false;
+  }
+
+  setUserDelete(username : string){
+    this.userToDelete = username;
+  }
+
+  deleteUser(){
+    this.userService.deleteUser(this.userToDelete).subscribe();
+    this.users = this.users.filter(User => User.username != this.userToDelete);
+    this.visible3 = false;
+  }
+
   ngOnInit() {
+    this.userService.getUsers().subscribe(data => this.users = data);
   }
 
   makeVisible(){
--- FrontEnd/Angular/CompanyVehicleService/src/app/user/user.service.ts
+++ FrontEnd/Angular/CompanyVehicleService/src/app/user/user.service.ts
@@ -13,22 +13,22 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     getUsers(): Observable<User[]>{
-        return this.http.get<User[]>('http://localhost:8080/finalProject/user');
+        return this.http.get<User[]>('http://localhost:8080/finalProject/users');
     }
 
     getUser(username: string): Observable<User>{
-        return this.http.get<User>('http:localhost:8080/finalProject/users' + username);
+        return this.http.get<User>('http://localhost:8080/finalProject/users/' + username);
     }
 
     postUser(user: User): Observable<User> {
-        return this.http.post<User>('http:localhost:8080/finalProject/users', user);
+        return this.http.post<User>('http://localhost:8080/finalProject/users', user);
     }
 
     deleteUser(username: string): Observable<{}> {
-        return this.http.delete('http:localhost:8080/finalProject/users' + username);
+        return this.http.delete('http://localhost:8080/finalProject/users/' + username);
     }
 
     putUser(user: User): Observable<User> {
-        return this.http.put<User>('http:localhost:8080/finalProject/users', user);
+        return this.http.put<User>('http://localhost:8080/finalProject/users', user);
     }
 }
